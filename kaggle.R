8394d472f0c7362705e2049bd1b88063dba921ac
Chun-Chung Yang <chunchung.yang@gmail.com>
2015-04-02 21:13:22 -0400

    Add AXA-Telematics

--- kaggle.R
+++ kaggle.R
@@ -3,10 +3,8 @@ dir="/Users/chunchung/Documents/Pitt/2015 spring/Data Mining/Project/drivers"
 submitPath="/Users/chunchung/Documents/Pitt/2015 spring/Data Mining/Project/data2"
 
 
-driver <- function(dir,submitPath,driverID,pathID){
+driver <- function(dir,submitPath,driverID){
   driverID=toString(driverID)
-  pathID=toString(pathID)
-  pathID=paste(pathID,"csv",sep=".")
   mainDir=paste(dir,driverID,sep='/')
   #setwd(mainDir)
   path(mainDir, driverID,submitPath)  
@@ -19,8 +17,10 @@ path <- function(mainDir,driverID,submitPath){
     
   readPath=paste(mainDir,pathID,sep='/')
   data=read.csv(readPath)
-  
-  sg <- savitzkyGolay(data, p = 0, w = 1, m = 0)
+  data = t(data)
+  data <- savitzkyGolay(data, p = 4, w = 21, m = 0)
+  data = data - data[,1]
+  data = data.frame(t(data.frame(data)))
   
   #Calculate the parameters
   distances=sqrt(diff(data$x)^2 + diff(data$y)^2)
@@ -46,6 +46,5 @@ path <- function(mainDir,driverID,submitPath){
 
 ## main function
 for(driverID in 1){
-    driver(dir,submitPath,driverID,pathID)
-  
+    driver(dir,submitPath,driverID)
 }
